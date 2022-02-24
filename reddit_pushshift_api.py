1c29b1cc705b710c3765ae179424aa21a0c4b521
Mandy Woo <mandywoo@dhcp-v079-156.mobile.uci.edu>
2022-02-23 17:51:15 -0800

    saved comments to json

--- reddit_pushshift_api.py
+++ reddit_pushshift_api.py
@@ -5,18 +5,20 @@ import time
 '''Get comments'''
 # supports s,m,h,d 
 time_frame = 'h'
-comments = []
+comment_lis = []
 for i in range(10):
     before = str(i) + time_frame
     after = str(i + 1) + time_frame
     url = 'https://api.pushshift.io/reddit/search/comment/?subreddit=recipes&after={}&before={}'.format(after, before)
     data = json.load(urlopen(url))['data']
     for comment_info in data:
-        comments.append(comment_info['body'])
+        comment_lis.append(comment_info['body'])
 
     # print(data)
     print(len(data))
 
     time.sleep(3)
 
-print(comments)
+print(comment_lis)
+with open('comments.json', 'w') as f:
+    json.dump(comment_lis, f)
