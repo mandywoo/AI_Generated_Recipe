0ab034cfc66d0d502111a0f1d33ccc47ce65c307
Mandy Woo <mandywoo@Mandys-MacBook-Pro-2.local>
2022-04-11 18:01:19 -0700

    deleted ingredients set

--- recipes1m_simplified.py
+++ recipes1m_simplified.py
@@ -2,13 +2,12 @@ import numpy as np
 from collections import defaultdict
 import pickle
 
-with np.load('simplified-recipes-1M.npz', allow_pickle=True) as data, open('ingredient_recipe_dict.pkl', 'wb') as handle1, open('unique_ingredients_set.pkl', 'wb') as handle2:
+with np.load('simplified-recipes-1M.npz', allow_pickle=True) as data, open('ingredient_recipe_dict.pkl', 'wb') as handle:
     
     recipes = data['recipes']
     ingredients = data['ingredients']
 
     ingredient_recipe_dict = defaultdict(int)
-    unique_ingredients_set = set()
 
     print(ingredients[recipes[0]])
     # print(recipes[0])
@@ -19,9 +18,7 @@ with np.load('simplified-recipes-1M.npz', allow_pickle=True) as data, open('ingr
         ingredient_lis = ingredients[recipe]
         for ingredient in ingredient_lis:
             ingredient_recipe_dict[ingredient] += 1
-            unique_ingredients_set.add(ingredient)
 
-    pickle.dump(ingredient_recipe_dict, handle1)
-    pickle.dump(unique_ingredients_set, handle2)
+    pickle.dump(ingredient_recipe_dict, handle)
 
     
--- visuals.py
+++ visuals.py
@@ -2,16 +2,13 @@ import pickle
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-with open('ingredient_recipe_dict.pkl', 'rb') as handle1, open('unique_ingredients_set.pkl', 'rb') as handle2:
-    ingredient_recipe_dict = pickle.load(handle1)
-    unique_ingredients_set = pickle.load(handle2)
+with open('ingredient_recipe_dict.pkl', 'rb') as handle:
+    ingredient_recipe_dict = pickle.load(handle)
 
     print(ingredient_recipe_dict['salt'])
-    print(len(unique_ingredients_set))
 
     # word cloud
-    ingredient_text = ' '.join(unique_ingredients_set)
-    word_cloud = WordCloud(collations=False, background_color='white').generate(ingredient_text)
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    plt.show()
+    wordcloud = WordCloud(width = 1000, height = 500).generate_from_frequencies(ingredient_recipe_dict)
+    plt.figure(figsize=(15,8))
+    plt.imshow(wordcloud)
+    plt.savefig('foo.png')
